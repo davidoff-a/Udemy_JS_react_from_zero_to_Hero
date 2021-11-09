fa186d33745abb1147662fbc01962134ffe9a80c
Алексей Давыдов <62559479+davidoff-a@users.noreply.github.com>
2021-11-09 15:19:40 +0300

    feat: add project components

--- crud/src/components/app/app.js
+++ crud/src/components/app/app.js
@@ -1,23 +1,21 @@
-import AppInfo from '../app-info/app-info';
-import SearchPanel from '../search-panel/search-panel';
-import AppFilter from '../app-filter/app-filter';
-import EmployeesList from '../employees-list/employees-list';
-import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import "./app.css"
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-import './app.css';
 
-function App() {
+function App(){
   return (
-    <div className="app">
-        <AppInfo />
-
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
+    <div className='app'>
+      <AppInfo />
+      <div className='search-panel'>
+        <SearchPanel />
+        <AppFilter />
+      </div>
+      <EmployeesList />
+      <EmployeesAddForm />
     </div>
   );
 }
--- crud/src/index.js
+++ crud/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './components/app/app';
+
+
 
 import './index.css';
 
